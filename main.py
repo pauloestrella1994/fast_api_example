652f11d71e456f45b5ee2a5c9df8923f38e42735
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-30 18:54:37 -0300

    Find by name in url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -17,3 +18,10 @@ async def root():
 @app.get("/get-student/{student_id}")
 async def get_student(student_id: int = Path(None, description="The id of the student", gt = 0, lt = 3)):
   return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(name: Optional[str] = None):
+  for student_id in students:
+    if students[student_id]["name"] == name:
+      return students[student_id]
+  return {"Data": "Not found"}
