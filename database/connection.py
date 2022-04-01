a1956dea73096db6937047ab632c9c3cce725d22
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-04-01 02:00:58 -0300

    Post user, jinja2 and routes

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from settings import settings
+from .settings import settings
 
 
 engine = create_async_engine(settings.database_url)
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from database.models import User
+from service import UserService
+from schemas import User
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/', response_class=HTMLResponse)
+async def root(request: Request):
+  return templates.TemplateResponse("root.html", {"request": request})
+
+@app.post('/user/create')
+async def create_user(user: User):
+  try:
+    await UserService.create_user(name=user.name)
+  except:
+    pass
