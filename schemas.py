2549ccb9366059e002e858a4519db54a733d1af8
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-04-01 02:13:40 -0300

    Add messages and user

--- schemas.py
+++ schemas.py
@@ -1,5 +1,12 @@
+from email import message
 from pydantic import BaseModel
 
 
 class User(BaseModel):
   name: str
+
+class Standard(BaseModel):
+  message: str
+
+class AlternativeOutput(Standard):
+  details: str
