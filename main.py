b0f7dea8b92282123d9da1e7615ae6c47d5c17e1
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-04-01 02:27:28 -0300

    Delete user

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ templates = Jinja2Templates(directory="templates")
 async def root(request: Request):
   return templates.TemplateResponse("root.html", {"request": request})
 
-@app.post('/user/create', description="Create User",response_model=Standard, responses={400: {'model': Error}})
+@app.post('/user/create', description="Create User", response_model=Standard, responses={400: {'model': Error}})
 async def create_user(user: User):
   try:
     await UserService.create_user(name=user.name)
+    return Standard(message="Ok")
+  except Exception as error:
+    raise HTTPException(400, detail=str(error))
+
+@app.delete('/delete/{user_id}', description="Delete User", response_model=Standard, responses={400: {'model': Error}})
+async def create_user(user_id: int):
+  try:
+    await UserService.delete_user(user_id)
     return Standard(message="Ok")
   except Exception as error:
     raise HTTPException(400, detail=str(error))
